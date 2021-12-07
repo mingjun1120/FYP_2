ec7403a78c536f1051b7e63023b62369b56060a2
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-12-07 21:55:06 +0800

    Update

--- main.py
+++ main.py
@@ -2,11 +2,6 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from upload_label_crop import main_upload_label_crop
-import streamlit as st
-import os
-from pdf2image import convert_from_path
-from PIL import Image
-from detect2 import main_detect2
 
 
 def main():
